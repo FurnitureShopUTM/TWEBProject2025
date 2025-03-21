02a41d8746734a784a2f5b864b3a914e38d64c6e
TimonV3 <163009472+TimonV3@users.noreply.github.com>
2025-03-21 12:09:53 +0200

    Чистый старт

---
+++ Project 2X/Controllers/HomeControler.cs
@@ -0,0 +1,60 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+public class HomeController : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+    public ActionResult About()
+    {
+        return View();
+    }
+
+    public ActionResult Home()
+    {
+        return View();
+    }
+
+    public ActionResult Promotions()
+    {
+        return View();
+    }
+
+    public ActionResult Credit()
+    {
+        return View();
+    }
+
+    public ActionResult ExamplesTextiles()
+    {
+        return View();
+    }
+
+    public ActionResult Delivery()
+    {
+        return View();
+    }
+
+    public ActionResult Payment()
+    {
+        return View();
+    }
+
+    public ActionResult Contact()
+    {
+        return View();
+    }
+
+    public ActionResult Basket()
+    {
+        return View();
+    }
+
+    public ActionResult LoginPage()
+    {
+        return View();
+    }
+}
